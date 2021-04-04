f7dfddd70523d4175c06b1e0f326bbab5ae83b7d
idris demir <idris.dmr@hotmail.com>
2021-04-05 01:53:05 +0300

    splash screen redirector is added.

--- app/src/main/java/com/idrisdemir/badapp/Welcome.java
+++ app/src/main/java/com/idrisdemir/badapp/Welcome.java
@@ -2,9 +2,7 @@ package com.idrisdemir.badapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -18,32 +16,19 @@ import com.idrisdemir.badapp.R;
  * status bar and navigation/system bar) with user interaction.
  */
 public class Welcome extends AppCompatActivity {
-    private int loginState = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
         ImageView iv= (ImageView) findViewById(R.id.imageView);
-        Animation uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
-        iv.setAnimation(uptodown);
+        Animation upToDown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        iv.setAnimation(upToDown);
 
         TextView tv = (TextView) findViewById(R.id.logo);
-        Animation downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
-        tv.setAnimation(downtoup);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent nextStage;
-                if (loginState == 0){
-                    nextStage = new Intent(Welcome.this,LoginActivity.class);
-                }else{
-                    nextStage = new Intent(Welcome.this,MainActivity.class);
-                }
-                startActivity(nextStage);
-            }
-        },2000);
+        Animation downToUp = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        tv.setAnimation(downToUp);
     }
 
 }
