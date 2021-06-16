f9e4e4d559b49dbd23cd198b0e17c5331ec1fa71
anilerkut <erkutanil.dinc@gmail.com>
2021-06-16 13:36:54 +0300

    braincoin and energy count

--- app/src/main/java/com/idrisdemir/badapp/Fragments/QuizFragment.java
+++ app/src/main/java/com/idrisdemir/badapp/Fragments/QuizFragment.java
@@ -2,11 +2,14 @@ package com.idrisdemir.badapp.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.idrisdemir.badapp.R;
 
@@ -16,6 +19,9 @@ import com.idrisdemir.badapp.R;
  * create an instance of this fragment.
  */
 public class QuizFragment extends Fragment {
+
+    public String bCount="235";
+    public String eCount="5";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +67,16 @@ public class QuizFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_quiz,container,false);
         return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        TextView braincoin_count =(TextView) view.findViewById(R.id.brain_count);
+        TextView energy_count =(TextView) view.findViewById(R.id.energy_count);
+        braincoin_count.setText(bCount);
+        energy_count.setText(eCount);
     }
 }
