53992ca313d49170a5357689056f89f1e14a197a
idris demir <idris.dmr@hotmail.com>
2021-06-18 22:01:28 +0300

    Questions was got

--- app/src/main/java/com/idrisdemir/badapp/QuizActivity.java
+++ app/src/main/java/com/idrisdemir/badapp/QuizActivity.java
@@ -1,8 +1,10 @@
 package com.idrisdemir.badapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -10,92 +12,199 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.idrisdemir.badapp.Entity.Question;
 
-import java.lang.reflect.Array;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
 
     private ArrayList<Question> questionArrayList;
-    private Question quest;
-    TextView cardQuestion,optionA,optionB,optionC,optionD,questcount;
-    CardView card0A,card0B,card0C,card0D;
-    Button nextQuestionButton;
-    int index=0;
+    private TextView cardQuestion,optionA,optionB,optionC,optionD,questcount,timeTV;
+    private TextView[] optionsTextViewArray = new TextView[4];
+    private CardView card0A,card0B,card0C,card0D;
+    private Button nextQuestionButton;
+    private int index=0;
     private String[] options=new String[4];
-    String lastClicked=null;
+    private String lastClicked=null;
+    private DatabaseReference databaseReference;
+    private ArrayList<Question> selectedQuestion = new ArrayList<Question>();
 
     private CountDownTimer countDownTimer;
     private ProgressBar progressBar;
-    int timerValue=20;
 
-    private  String[] optionMaker(String[] options,String a,String b,String c,String d)
-    {
-        options[0]=a;
-        options[1]=b;
-        options[2]=c;
-        options[3]=d;
-        return options;
-    }
+    private int currentQuestion=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-
-        Hooks();
-        questionArrayList=new ArrayList<Question>();
-        options=optionMaker(options,"tr1","eng1","brit1","sivas1");
-        questionArrayList.add(new Question("1ankara nerenin baskenti",50,options,111,"History"));
-        options=optionMaker(options,"tr2","eng2","brit2","sivas2");
-        questionArrayList.add(new Question("2ankara nerenin baskenti",50,options,111,"History"));
-        options=optionMaker(options,"tr3","eng3","brit3","sivas3");
-        questionArrayList.add(new Question("3ankara nerenin baskenti",50,options,111,"History"));
-        options=optionMaker(options,"tr4","eng4","brit4","sivas4");
-        questionArrayList.add(new Question("4ankara nerenin baskenti",50,options,111,"History"));
-        questcount.setText(index+1 +"/"+questionArrayList.size());
-        //Collections.shuffle(questionArrayList);
-        quest=questionArrayList.get(index);
-        timerValue=quest.getTime();
-        //setQuestion();
+        connectAllElements();
         enableButtons();
-        //Timer
-        TextView time=findViewById(R.id.timeText);
-        progressBar.setMax(timerValue);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
-        countDownTimer=new CountDownTimer(timerValue*1000,1000) {
+        Query query = databaseReference.child("questions")
+                            .orderByChild("categoryName").equalTo("Tarih");
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onTick(long millisUntilFinished) {
-                timerValue=timerValue-1;
-                time.setText(String.valueOf(timerValue));
-                progressBar.setProgress(timerValue);
-            }
-            @Override
-            public void onFinish() {
-                if(index<questionArrayList.size()-1) {
-                    index++;
-                    questcount.setText(index+1 +"/"+questionArrayList.size());
-                    quest = questionArrayList.get(index);
-                    timerValue = quest.getTime();
-                    resetColor();
-                    //setQuestion();
-                    countDownTimer.start();
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                ArrayList<Question> tempList = new ArrayList<Question>();
+                for (DataSnapshot ss:snapshot.getChildren()) {
+                    tempList.add(ss.getValue(Question.class));
                 }
-                else
-                    EndGame();
+                startGame(tempList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
 
             }
-        }.start();
+        });
+    }
+
+    private void startGame(ArrayList<Question> questions){
+        int i = 1;
+        Collections.shuffle(questions);
+        for (Question q:questions){
+            selectedQuestion.add(q);
+            if (i==10) break;
+            i++;
+        }
+        setQuestion(selectedQuestion.get(0));
+    }
+
+    private void setQuestion(Question question) {
+        ArrayList<Integer> added = new ArrayList<Integer>();
+        ArrayList<String> answers = new ArrayList<String>();
+        answers.add(question.getOption1());
+        answers.add(question.getOption2());
+        answers.add(question.getOption3());
+        answers.add(question.getOption4());
+        Random random = new Random();
+        for (int i = 0; i < 4; i++) {
+            while (true) {
+                int randomLocation = random.nextInt(4) ;
+                if (!added.contains(randomLocation)){
+                    optionsTextViewArray[randomLocation].setText(answers.get(i));
+                    added.add(randomLocation);
+                    break;
+                }
+            }
+        }
+        cardQuestion.setText(question.getQuestion());
+        progressBar.setMax(question.getTime());
+        counterManage(question.getTime() + 1);
+        questcount.setText(String.valueOf(currentQuestion + 1) + " / " + String.valueOf(selectedQuestion.size()));
 
     }
 
-    private void Hooks() {
+    public void nextButtonClicked(View view){
+        countDownTimer.cancel();
+        resetColorToBlueWhite();
+
+        currentQuestion++;
+        if (currentQuestion == selectedQuestion.size()) {
+            endGame();
+            return;
+        }
+
+        setQuestion(selectedQuestion.get( currentQuestion ));
+    }
+
+    public void enableButtons(){
+        card0A.setClickable(true);
+        card0B.setClickable(true);
+        card0C.setClickable(true);
+        card0D.setClickable(true);
+    }
+    public void resetColorToBlueWhite(){
+        card0A.setCardBackgroundColor(getResources().getColor(R.color.white));
+        card0B.setCardBackgroundColor(getResources().getColor(R.color.white));
+        card0C.setCardBackgroundColor(getResources().getColor(R.color.white));
+        card0D.setCardBackgroundColor(getResources().getColor(R.color.white));
+    }
+
+
+    private void counterManage(int timerValue){
+        countDownTimer=new CountDownTimer(timerValue*1000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int currentSecond = (int) (millisUntilFinished/1000);
+                timeTV.setText(String.valueOf(currentSecond));
+                progressBar.setProgress(currentSecond);
+            }
+            @Override
+            public void onFinish() {
+                timeIsUp();
+            }
+        }.start();
+    }
+
+    private void timeIsUp() {
+        nextButtonClicked(nextQuestionButton);
+    }
+
+    public void endGame() {
+        Intent intent=new Intent(QuizActivity.this,ScoreScreenActivity.class);
+        startActivity(intent);
+    }
+    public void optionA_clicked(View view) {
+        if(lastClicked=="A")
+        {
+            resetColorToBlueWhite();
+            lastClicked=null;
+        }else{
+            resetColorToBlueWhite();
+            card0A.setCardBackgroundColor(getResources().getColor(R.color.libYellow2x));
+            lastClicked="A";
+        }
+
+    }
+    public void optionB_clicked(View view) {
+        if(lastClicked=="B")
+        {
+            resetColorToBlueWhite();
+            lastClicked=null;
+        }else{
+            resetColorToBlueWhite();
+            card0B.setCardBackgroundColor(getResources().getColor(R.color.libYellow2x));
+            lastClicked="B";
+        }
+    }
+    public void optionC_clicked(View view) {
+        if(lastClicked=="C") {
+            resetColorToBlueWhite();
+            lastClicked=null;
+        }else {
+            resetColorToBlueWhite();
+            card0C.setCardBackgroundColor(getResources().getColor(R.color.libYellow2x));
+            lastClicked="C";
+        }
+    }
+    public void optionD_clicked(View view) {
+        if(lastClicked=="D") {
+            resetColorToBlueWhite();
+            lastClicked=null;
+        }else{
+            resetColorToBlueWhite();
+            card0D.setCardBackgroundColor(getResources().getColor(R.color.libYellow2x));
+            lastClicked="D";
+        }
+    }
+    private void connectAllElements() {
         questcount=findViewById(R.id.questionCount);
         progressBar=findViewById(R.id.time_progress);
         cardQuestion=findViewById(R.id.questionCard);
@@ -107,124 +216,11 @@ public class QuizActivity extends AppCompatActivity {
         card0B=findViewById(R.id.cardB);
         card0C=findViewById(R.id.cardC);
         card0D=findViewById(R.id.cardD);
+        timeTV=findViewById(R.id.timeText);
         nextQuestionButton=findViewById(R.id.next_question);
-    }/*
-    private void setQuestion() {
-        cardQuestion.setText(quest.getQuestion());
-        optionA.setText(quest.getOptions(0));
-        optionB.setText(quest.getOptions(1));
-        optionC.setText(quest.getOptions(2));
-        optionD.setText(quest.getOptions(3));
+        optionsTextViewArray[0]= optionA;
+        optionsTextViewArray[1]= optionB;
+        optionsTextViewArray[2]= optionC;
+        optionsTextViewArray[3]= optionD;
     }
-    */
-
-    /*
-    public void Answer()
-    {
-        nextQuestionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(index<questionArrayList.size()-1)
-                {
-                    index++;
-                    quest=questionArrayList.get(index);
-                    setQuestion();
-                }
-                else
-                    EndGame();
-            }
-        });
-    }
-    */
-    public void  nextclicked(View view)
-    {
-        if(index<questionArrayList.size()-1)
-        {
-            index++;
-            questcount.setText(index+1 +"/"+questionArrayList.size());
-            quest=questionArrayList.get(index);
-            timerValue=quest.getTime();
-            resetColor();
-            //setQuestion();
-
-        }
-        else
-            EndGame();
-    }
-    public void EndGame()
-    {
-        Intent intent=new Intent(QuizActivity.this,ScoreScreenActivity.class);
-        startActivity(intent);
-    }
-    public void enableButtons(){
-        card0A.setClickable(true);
-        card0B.setClickable(true);
-        card0C.setClickable(true);
-        card0D.setClickable(true);
-    }
-    public void resetColor(){
-        card0A.setCardBackgroundColor(getResources().getColor(R.color.white));
-        card0B.setCardBackgroundColor(getResources().getColor(R.color.white));
-        card0C.setCardBackgroundColor(getResources().getColor(R.color.white));
-        card0D.setCardBackgroundColor(getResources().getColor(R.color.white));
-    }
-    public void optionA_clicked(View view)
-    {
-        if(lastClicked=="A")
-        {
-            resetColor();
-            lastClicked=null;
-        }
-        else
-        {
-            resetColor();
-            card0A.setCardBackgroundColor(getResources().getColor(R.color.libYellow2x));
-            lastClicked="A";
-        }
-
-    }
-    public void optionB_clicked(View view)
-    {
-        if(lastClicked=="B")
-        {
-            resetColor();
-            lastClicked=null;
-        }
-        else
-        {
-            resetColor();
-            card0B.setCardBackgroundColor(getResources().getColor(R.color.libYellow2x));
-            lastClicked="B";
-        }
-    }
-    public void optionC_clicked(View view)
-    {
-        if(lastClicked=="C")
-        {
-            resetColor();
-            lastClicked=null;
-        }
-        else
-        {
-            resetColor();
-            card0C.setCardBackgroundColor(getResources().getColor(R.color.libYellow2x));
-            lastClicked="C";
-        }
-    }
-    public void optionD_clicked(View view)
-    {
-        if(lastClicked=="D")
-        {
-            resetColor();
-            lastClicked=null;
-        }
-        else
-        {
-            resetColor();
-            card0D.setCardBackgroundColor(getResources().getColor(R.color.libYellow2x));
-            lastClicked="D";
-        }
-    }
-
-
 }
