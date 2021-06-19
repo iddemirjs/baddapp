5be56d4baec7689b48883ac28497f0ba8b4cf560
anilerkut <erkutanil.dinc@gmail.com>
2021-06-19 23:10:55 +0300

    exit button has been completed

--- app/src/main/java/com/idrisdemir/badapp/ScoreScreenActivity.java
+++ app/src/main/java/com/idrisdemir/badapp/ScoreScreenActivity.java
@@ -2,8 +2,14 @@ package com.idrisdemir.badapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.idrisdemir.badapp.Fragments.HomeFragment;
+import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
 
 public class ScoreScreenActivity extends AppCompatActivity {
     private TextView score,time;
@@ -13,5 +19,15 @@ public class ScoreScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_score_screen);
         score=findViewById(R.id.score_text);
         time=findViewById(R.id.time_text);
+        Button rewardOutButton = (Button) findViewById(R.id.exit_winner_screen);
+        rewardOutButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent goHomeFragment=new Intent(ScoreScreenActivity.this, HomeFragment.class);
+                startActivity(goHomeFragment);
+            }
+        });
     }
 }
