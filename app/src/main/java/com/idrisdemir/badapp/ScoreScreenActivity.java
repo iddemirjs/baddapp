304d08dba3a5ca2eda94b31d76dd250f92e910bd
derviscomlekci <derviscomlek@gmail.com>
2021-06-19 17:56:20 +0300

    Score screen is done.

--- app/src/main/java/com/idrisdemir/badapp/ScoreScreenActivity.java
+++ app/src/main/java/com/idrisdemir/badapp/ScoreScreenActivity.java
@@ -3,12 +3,15 @@ package com.idrisdemir.badapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ScoreScreenActivity extends AppCompatActivity {
-
+    private TextView score,time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_screen);
+        score=findViewById(R.id.score_text);
+        time=findViewById(R.id.time_text);
     }
 }
