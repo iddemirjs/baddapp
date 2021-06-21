80c6015a92e28bc20297a7f3bd62b09337da79e3
idris demir <idris.dmr@hotmail.com>
2021-06-21 19:29:14 +0300

    new updated

--- app/src/main/java/com/idrisdemir/badapp/QuizActivity.java
+++ app/src/main/java/com/idrisdemir/badapp/QuizActivity.java
@@ -69,6 +69,7 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
         connectAllElements();
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
