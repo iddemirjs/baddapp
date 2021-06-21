92c9a27c608d144af52a3379982462237026da9d
anilerkut <erkutanil.dinc@gmail.com>
2021-06-21 10:47:55 +0300

    member class has been done

--- app/src/main/java/com/idrisdemir/badapp/Entity/Member.java
+++ app/src/main/java/com/idrisdemir/badapp/Entity/Member.java
@@ -39,4 +39,13 @@ public class Member extends User {
     public void setLevel(int level) {
         this.level = level;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
