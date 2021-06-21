71fbef6a584d8a661e7b28303d334a374ae207ac
idris demir <idris.dmr@hotmail.com>
2021-06-21 14:06:19 +0300

    Game Engine and User Manager has ben created.

---
+++ app/src/main/java/com/idrisdemir/badapp/SystemEngine/UserManagement.java
@@ -0,0 +1,9 @@
+package com.idrisdemir.badapp.SystemEngine;
+
+public class UserManagement {
+
+    public void levelUppControl(String userName){
+        // TODO : Increase User Level if he reach next level exprience;
+    }
+
+}
